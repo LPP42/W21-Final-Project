5a438815c5733e94171ed5dcb05a43a0bae64a78
LPP <2175801@cegep-heritage.qc.ca>
2022-04-08 10:53:30 -0400

    added rec age

--- Model/Product.cs
+++ Model/Product.cs
@@ -1,6 +1,11 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using FoolProof.Core;
+using Microsoft.AspNetCore.Mvc;
+
+
 namespace shoptry.Models;
+
 
 public enum ProductCategory {Any, Board, Computer, Lego, Plastic, Doll, Plushies, Art}
 public class Product
@@ -13,8 +18,10 @@ public class Product
     public decimal Price { get; set; } = 1000.00M;
     public uint Stock { get; set; } = 0;
     public ProductCategory Category { get; set; }
-    [Column(TypeName = "decimal(2,1)")]
-    public decimal? RecAgeMin {get;set;}   
-    [Column(TypeName = "decimal(2,1)")]
+    [Column(TypeName = "decimal(3,1)")]
+    [LessThanOrEqualTo("RecAgeMax",ErrorMessage ="Min age must be less equal or Max age")]
+    public decimal? RecAgeMin {get;set;} 
+    [GreaterThanOrEqualTo("RecAgeMin",ErrorMessage ="Min age must be less equal or Max age")]  
+    [Column(TypeName = "decimal(3,1)")]
     public decimal? RecAgeMax {get;set;}   
 }
