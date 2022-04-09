7d35acba560e71502b3aa55504a3fcf57984eaae
salwa <1496118@cegep-heritage.qc.ca>
2022-04-08 22:40:04 -0400

    added para img

---
+++ Migrations/20220408232141_newdatacart1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace shoptry.Migrations
+{
+    public partial class newdatacart1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
